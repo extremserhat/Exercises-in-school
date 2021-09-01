161853d01af1730f79f3d37d803d6c4e510c719d
Serhat Leylek <79966602+extremserhat@users.noreply.github.com>
2021-09-01 15:47:35 +0200

    Add files via upload

---
+++ LF06c/Tag04/04_alle-formularfelder/contact.php
@@ -0,0 +1,11 @@
+<?php
+
+if (isset($_REQUEST) && !empty($_REQUEST)) {
+  echo (!empty($_POST)) ? 'Daten per POST erhalten' : 'Daten per GET erhalten';
+
+  echo '<pre>';
+  echo print_r($_REQUEST);
+  echo '</pre>';
+} else {
+  echo 'Keine Daten erhalten.';
+}
